36077069a00c4adb26c0f035b5a95b85528d046d
AmMaikliii <youkeyam@gmail.com>
2026-03-03 13:13:29 -0600

    arrow pop-ups

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,12 +6,41 @@ using UnityEngine.SceneManagement;
 public class Wall : MonoBehaviour
 {
     public string nextLevel = "";
+    public SpriteRenderer render;
+    public Transform transform;
+
+    public float amount = 1f;
+
+    public Color visible = new Color(255, 255, 255, 255);
+    public Color transparent = new Color(0, 0, 0, 0);
+
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        transform = GetComponent<Transform>();
+
+        render.color = transparent;
+    }
+
+    void Update()
+    {
+        /*Vector3 move = Vector3.one * amount;
+        for (int i = 0; i < 5; i++)
+        {
+            transform.position += move;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            transform.position -= move;
+        }*/
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            print("Next Level: Wall");
+            print("(Wall) Next Level");
+            render.color = visible;
             Invoke("NextLevel", 1);
         }
     }
